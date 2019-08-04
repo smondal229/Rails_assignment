67fc9e9b41ed2b39f888fd1415ca196b9d632a7d
Suvodip Mondal <smondal229@gmail.com>
2019-08-04 10:14:44 +0530

    indentation fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   def self.to_csv
     CSV.generate do |csv|
       csv << User.column_names
-        all.each do |user|
-          csv<< user.attributes.values_at(*User.column_names)
-        end
+      all.each do |user|
+        csv<< user.attributes.values_at(*User.column_names)
+      end
     end
   end
 end
