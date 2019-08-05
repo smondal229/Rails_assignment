efe4f4dec3cc5ebaeb246c435f3e8afddd27d018
Suvodip Mondal <smondal229@gmail.com>
2019-08-05 23:58:07 +0530

    DRY customers_controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,6 @@
 class CustomersController < ApplicationController
     #CRUD operation on customer
+  before_action :find_param_id,  only: [:show, :edit, :destroy, :update]
   def index
     if params[:q].present?
       @keyword=params[:q].to_s.strip.downcase
@@ -25,27 +26,22 @@ class CustomersController < ApplicationController
 
 
   def show
-    @customer=Customer.find(params[:id])
   end
 
   def edit
-    @customer=Customer.find(params[:id])
   end
   def update
-    @customer=Customer.find(params[:id])
     if @customer.update(customer_params)
       redirect_to customers_path, flash: { success: "Customer record updated successfully" }
     else
       flash[:error]=@customer.errors.full_messages.to_sentence
-      puts(@customer.errors.full_messages.to_sentence)
       render action: "edit"
     end
   end
 
   def destroy
-    @customer=Customer.find(params[:id])
     @customer.destroy
-    redirect_to(customers_path, flash: { success: "Customer record deleted successfully" } )
+    redirect_to(customers_path, flash: { success: "Customer \"#{@customer.name}\" deleted successfully" } )
   end
   
   private
@@ -53,4 +49,7 @@ class CustomersController < ApplicationController
       params.require(:customer).permit(:name,:email)
     end
 
+    def find_param_id
+      @customer=Customer.find(params[:id])
+    end
 end
